5113ce2c4e08063bfcb6629de9946157a110f301
Mohammed Aamir Mansoori <aamir.mansoori2911@gmail.com>
2023-12-06 22:56:17 +0530

    Apple's responsive nav created

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,39 +4,167 @@ import { useState, useEffect } from "react";
 import applelogo from "../assets/images/applelogo.png";
 import searchimg from "../assets/images/search.png";
 import bagimag from "../assets/images/bag.png";
+import hamburgerIcon from "../assets/images/hamburger.png";
+import close from "../assets/images/close.png";
 
 const NavBar = () => {
     const [scrolled, setScrolled] = useState(false);
+    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 830);
+    const [isMenuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
             const isScrolled = window.scrollY > 44;
             setScrolled(isScrolled);
         };
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth < 830);
+        };
 
         window.addEventListener("scroll", handleScroll);
+        window.addEventListener("resize", handleResize);
 
         // Cleanup the event listener on component unmount
         return () => {
             window.removeEventListener("scroll", handleScroll);
+            window.removeEventListener("resize", handleResize);
         };
     }, []);
+
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
+    const closeMenu = () => {
+        // Close the menu
+        setMenuOpen(false);
+    };
+
     return (
         <div className={`NavBar ${scrolled ? "scrolled" : ""}`}>
             <div className="NavList">
-                <img src={applelogo} alt="logo" />
+                <img className="applelogo" src={applelogo} alt="logo" />
                 <div className="NavListStore">Store</div>
                 <div className="NavListMac">Mac</div>
                 <div className="NavListiPad">iPad</div>
                 <div className="NavListiPhone">iPhone</div>
                 <div className="NavListWatch">Watch</div>
                 <div className="NavListAirPods">AirPods</div>
-                <div className="NavListTV&Home">TV & Home</div>
+                <div className="NavListTVandHome">TV & Home</div>
                 <div className="NavListEntertainment">Entertainment</div>
                 <div className="NavListAccessories">Accessories</div>
                 <div className="NavListSupport">Support</div>
-                <img src={searchimg} alt="search" />
-                <img src={bagimag} alt="bag" />
+                {isSmallScreen ? (
+                    <>
+                        <div className="NavListIcons">
+                            <img
+                                className="NLIsearch"
+                                src={searchimg}
+                                alt="search"
+                            />
+                            <img className="NLIbag" src={bagimag} alt="bag" />
+                            <img
+                                src={hamburgerIcon}
+                                alt="hamburger-menu"
+                                className={`HamburgerIcon ${
+                                    isMenuOpen ? "open" : ""
+                                }`}
+                                onClick={toggleMenu}
+                            />
+                            {isMenuOpen && (
+                                <div
+                                    className={`MobileMenu ${
+                                        isMenuOpen ? "open" : ""
+                                    }`}
+                                >
+                                    {/* Add your mobile menu items here */}
+                                    <div
+                                        className="MenuItem closeButton"
+                                        onClick={closeMenu}
+                                    >
+                                        <img src={close} alt="Close" />
+                                    </div>
+                                    <div
+                                        className="MenuItem"
+                                        onClick={closeMenu}
+                                    >
+                                        Store
+                                    </div>
+                                    <div
+                                        className="MenuItem"
+                                        onClick={closeMenu}
+                                    >
+                                        Mac
+                                    </div>
+                                    <div
+                                        className="MenuItem"
+                                        onClick={closeMenu}
+                                    >
+                                        iPad
+                                    </div>
+                                    <div
+                                        className="MenuItem"
+                                        onClick={closeMenu}
+                                    >
+                                        iPhone
+                                    </div>
+                                    <div
+                                        className="MenuItem"
+                                        onClick={closeMenu}
+                                    >
+                                        Watch
+                                    </div>
+                                    <div
+                                        className="MenuItem"
+                                        onClick={closeMenu}
+                                    >
+                                        AirPods
+                                    </div>
+                                    <div
+                                        className="MenuItem"
+                                        onClick={closeMenu}
+                                    >
+                                        TV & Home
+                                    </div>
+                                    <div
+                                        className="MenuItem"
+                                        onClick={closeMenu}
+                                    >
+                                        Entertainment
+                                    </div>
+                                    <div
+                                        className="MenuItem"
+                                        onClick={closeMenu}
+                                    >
+                                        Accessories
+                                    </div>
+                                    <div
+                                        className="MenuItem"
+                                        onClick={closeMenu}
+                                    >
+                                        Support
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <div className="NavListStore">Store</div>
+                        {/* ... (rest of the NavList items) */}
+                        <div className="NavListIcons">
+                            <img
+                                className="NLIsearch"
+                                src={searchimg}
+                                alt="search"
+                            />
+                            <img
+                                className="NLIbag"
+                                src={bagimag}
+                                alt="bag"
+                            />
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     );
